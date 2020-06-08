9b6106ad92d2ce800c6530997601107eadb4f99e
Adrian Anton <adrtler@gmail.com>
2020-06-08 12:35:27 +0200

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from "react";
+import Styled from "@emotion/styled";
+import Frase from './components/Frase';
+
+const Contenedor = Styled.div`
+	display: flex;
+	align-items: center;
+	padding-top: 5rem;
+	flex-direction: column;
+`;
+
+const Boton = Styled.button`
+	background: -webkit-linear-gradient(
+		top left,
+		#007d35 0%,
+		#007d35 44%,
+		#0f574e 100%
+	);
+	background-size: 300px;
+	font-family: Arial, Helvetica, sans-serif;
+	color: #fff;
+	margin-top: 3rem;
+	padding: 1rem 3rem;
+	font-size: 2rem;
+	border: 2px solid black;
+	transition: background-size .8s ease;
+	:hover{
+		cursor:pointer;
+		background-size:400px;
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	// state de frases
+	const [frase, guardarFrase] = useState({
+
+	});
+
+	useEffect(()=>{
+		consultarAPI();
+	}, []);
+	const consultarAPI = async() => {
+		const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+		console.log(api);
+		const frase = await api.json();
+		guardarFrase(frase[0]);
+	};
+/*
+	const consultarAPI = () => {
+		const api = fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+		console.log(api);
+		const frase = api.then( respuesta =>{
+			return respuesta.json();
+		});
+		frase.then(resultado =>{
+			console.log(resultado);
+		})
+		console.log(frase);
+	};*/
+	// <Boton onClick={() => consultarAPI()}>Obtener frase</Boton>
+	return (
+		<Contenedor>
+			<Frase
+				frase={frase}
+			/>
+			<Boton onClick={consultarAPI}>Obtener frase</Boton>
+		</Contenedor>
+	);
 }
 
 export default App;
